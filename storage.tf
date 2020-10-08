acaf784edc9438461959512da92d3e7fbe5844cf
Tim Leguijt <t.leguijt@labdigital.nl>
2020-10-08 10:41:28 +0200

    Add terraform storage account

--- storage.tf
+++ storage.tf
@@ -1,13 +1,18 @@
 locals {
-  storage_account_name = replace(
+  components_sa_name = replace(
     lower(format("%s-sa-components", var.name_prefix)),
+    "-",
+    ""
+  )
+  terra_sa_name = replace(
+    lower(format("%s-sa-terra", var.name_prefix)),
     "-",
     ""
   )
 }
 
 resource "azurerm_storage_account" "components" {
-  name                     = local.storage_account_name
+  name                     = local.components_sa_name
   resource_group_name      = azurerm_resource_group.shared.name
   location                 = azurerm_resource_group.shared.location
   account_kind             = "BlockBlobStorage"
@@ -22,3 +27,22 @@ resource "azurerm_storage_container" "code" {
   storage_account_name  = azurerm_storage_account.components.name
   container_access_type = "blob"
 }
+
+
+resource "azurerm_storage_account" "terraform" {
+  name                     = local.terra_sa_name
+  resource_group_name      = azurerm_resource_group.shared.name
+  location                 = azurerm_resource_group.shared.location
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "RAGZRS"
+  tags = var.tags
+}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.terraform.name
+  container_access_type = "private"
+}
+
+
